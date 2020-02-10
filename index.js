3d16b147a1517d3e0e13d6a6e03eefc55174869d
Basruhe <sb.ruhe@gmail.com>
2020-02-10 10:30:54 +0100

    added start script

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ app.listen(port, () => {
   // console.log(`Express server listening on ${port}`);
   console.log("Express server listening on", port);
 });
+
+// Postgres server port: 5432
+// Host port: 3000
+// Express server port: 4000
