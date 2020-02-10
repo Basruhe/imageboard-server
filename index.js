fc46b62e411ddae4dee2b133bfc21a97280c0bba
Basruhe <sb.ruhe@gmail.com>
2020-02-10 12:05:35 +0100

    added findall route

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const port = process.env.PORT || 4000;
 const app = express();
 const db = require("./db");
 const Image = require("./image/model");
+const imageRouter = require("../server/router");
+
+app.use(imageRouter);
 
 app.listen(port, () => {
   // console.log(`Express server listening on ${port}`);
--- router.js
+++ router.js
@@ -1,2 +1,21 @@
-// const { Router } = require("express");
-// // const Image = require ("./")
+const { Router } = require("express");
+const Image = require("../server/image/model");
+const router = new Router();
+const bodyParser = require("body-parser");
+
+router.get(
+  "/image",
+  (request, response, next) =>
+    Image.findAll().then(images => {
+      response.json(images);
+      console.log("Message from findaAll at 4000/image");
+    })
+  // // this doesnt work: why?
+  // .then(
+  //   response.json({
+  //     message: "images requested"
+  //   })
+  // )
+);
+
+module.exports = router;
